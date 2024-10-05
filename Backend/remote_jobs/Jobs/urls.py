a3d1b87dbc0b24f98a574c7e48206b7385949255
Blessman-Newton <blessmannewton0@gmail.com>
2024-10-05 23:35:36 +0000

    update urls configurations

--- Backend/remote_jobs/Jobs/urls.py
+++ Backend/remote_jobs/Jobs/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import JobList, JobSeekerDetail
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('jobs/', JobList.as_view(), name='job-list'),
