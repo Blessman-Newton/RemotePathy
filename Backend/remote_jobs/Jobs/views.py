f1d422aff3c28e00757f03558cc54669280bd687
Blessman-Newton <blessmannewton0@gmail.com>
2024-10-05 23:34:43 +0000

    Added home.html file

--- Backend/remote_jobs/Jobs/views.py
+++ Backend/remote_jobs/Jobs/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .models import Job, JobSeeker
 from .serializers import JobSerializer, JobSeekerSerializer
+from django.shortcuts import render
 
 class JobList(generics.ListCreateAPIView):
     queryset = Job.objects.all()
@@ -9,3 +10,10 @@ class JobList(generics.ListCreateAPIView):
 class JobSeekerDetail(generics.RetrieveUpdateAPIView):
     queryset = JobSeeker.objects.all()
     serializer_class = JobSeekerSerializer
+
+
+
+
+def home(request):
+    return render(request, 'home.html')
+
