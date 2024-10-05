82569cf3c8a899fa4e69b56e2528623b13162d2d
Blessman-Newton <blessmannewton0@gmail.com>
2024-10-05 23:31:56 +0000

    urls configuration

--- Backend/remote_jobs/remote_jobs/urls.py
+++ Backend/remote_jobs/remote_jobs/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from Jobs import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('Jobs.urls')),
     path('accounts/', include('allauth.urls')),
+    path('', views.home, name='home'),
+
 ]
